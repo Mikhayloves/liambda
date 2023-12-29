99fe32d6e8562af8d1898f766c8c9c74cb8482fb
Neto test <toni_mikhaylovskiy@mail.ru>
2023-12-29 09:38:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.sql.SQLOutput;
+import java.util.*;
+import java.util.function.Supplier;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static Supplier<Calculator> calc = Calculator::new;
+
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+
+
+        try {
+            int a = calc.plus.apply(1, 2);
+            int b = calc.minus.apply(1, 1);
+            int c = calc.devide.apply(4, 0);
+            calc.println.accept(c);
+        } catch (Exception exception) {
+            System.out.printf(("Делить на ноль нельзя!\n " + " Ошибка =>" + exception.getMessage()));
+        }
+    }
+}
+
+
+
+
+
